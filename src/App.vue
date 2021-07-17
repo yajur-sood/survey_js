c84a5c5bd7c8ec911d4150efaefb94c0e0147508
unknown <yajur.sood@kennect.io>
2021-07-17 21:36:25 +0530

    choicesByUrl for dropdown, panelDynamic for multiple survey sets, clean code

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,8 @@ export default {
   name: 'App',
   data(){
     return {
-      states: [],
-      cities: [],
       products: [],
-      price:0,
-      customQuantity:0,
-      survey:null
+      survey:null,
     }
   },
   
@@ -28,109 +24,7 @@ export default {
     Survey
   },
   
-  beforeMount() {  
-    const headers = new Headers();
-    headers.append("X-CSCAPI-KEY", "YXFqZEJsWWpEbFFzSFNBb0ZKNkhTRVZ2SEhlc1dxUTBsOEd3dlhERA==");
-    const requestOptions = {
-      method: 'GET',
-      headers: headers,
-      redirect: 'follow'
-    };
-    
-    fetch("https://api.countrystatecity.in/v1/countries/IN/states", requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        result.forEach(state => {
-          this.states.push({text:state.name, value:state.iso2})
-        })
-      })
-      .then(()=>{
-        const surveyValueChanged=(sender,options)=>{
-          let max,customer
-
-          if(options.value===model.getValue('quantity')){
-            options.value = parseInt(options.value)
-          }
-          
-          if(model.getValue('products')){
-            max = parseInt(model.getValue('products').maximum)
-            this.price=parseInt(model.getValue('products').cost)
-            // console.log(max,this.price,"products value")
-          }
-          if(model.getValue('quantity')){
-            customer = parseInt(model.getValue('quantity'))
-            if(customer>max){
-              alert("We do not have that much in stock. Replacing your quantity with the maximum we have")
-              model.setValue('quantity',max)
-              
-            }
-            model.setValue('calculatedCost',customer*this.price)
-            // console.log(customer,"quantity")
-          }
-
-          if(!Number.isInteger(options.value)&&options.value.length<=2){
-            this.getCity(options.value, json)
-          }
-        }
-        let json = {
-          questions: [
-            {
-                type: "dropdown",
-                name: "state",
-                title: "select state",
-                isRequired: true,
-                colCount: 0,
-                choices:this.states
-            },
-            {
-              type: "dropdown",
-                name: "cities",
-                title: "select city",
-                isRequired: true,
-                colCount: 0,
-                choices: this.cities
-            },
-            {
-              type: "dropdown",
-              name: "products",
-              title: "select product",
-              isRequired: true,
-              colCount: 0,
-              choices: this.products
-            },
-            {
-              type: "text",
-              name: "quantity",
-              title: "how much?",
-              isRequired: true,
-              colCount: 0,
-              // inputType:"int",
-              validators: [
-                {
-                    type: "numeric",
-                }
-              ],
-            },
-            {
-              type: "text",
-              mode: "display",
-              readOnly: true,
-              name: "calculatedCost",
-              title: "total cost",
-              colCount: 0,
-            }
-          ]
-        } 
-
-        const model = new SurveyVue.Model(json)
-        this.survey = model
-        
-        model
-        .onValueChanged
-        .add(surveyValueChanged)
-      })
-      .catch(error => console.log('error', error));
-    
+  beforeMount() {
     axios.get("http://localhost:3000")
     .then(response=>{
       let prod = []
@@ -138,40 +32,114 @@ export default {
       prod.forEach(item=>{
         this.products.push({text: item.product, cost:parseInt(item.cost), maximum:parseInt(item.max_quantity)})
       })
-    })    
-  },
+    })
+      .then(()=>{
+        let json = {
+          showQuestionNumbers: "off",
+          elements: [
+            {
+              type: "paneldynamic",
+              name: "items",
+              title: "Items",
+              keyName: "name",
+              showQuestionNumbers: "off",
+              templateTitle: "item #{panelIndex}",
+              templateElements: [
+                  {
+                      type: "dropdown",
+                      name: "state",
+                      title: "select state",
+                      isRequired: true,
+                      colCount: 0,
+                      choicesByUrl: {
+                        url: "https://api.countrystatecity.in/v1/countries/IN/states",
+                        titleName: "name",
+                        valueName: "iso2"
+                      }
+                  },
+                  {
+                    type: "dropdown",
+                      name: "cities",
+                      title: "select city",
+                      isRequired: true,
+                      colCount: 0,
+                      choicesByUrl: {
+                        url: 'https://api.countrystatecity.in/v1/countries/IN/states/{panel.state}/cities',
+                        valueName: 'name'
+                      }
+                  },{
+                      type: "dropdown",
+                      name: "products",
+                      title: "select product",
+                      isRequired: true,
+                      colCount: 0,
+                      choices: this.products
+                    }, {
+                      type: "text",
+                      name: "quantity",
+                      inputType: "number",
+                      title: "Quantity:",
+                      isRequired: true,
+                      startWithNewLine: false,
+                      validators: [
+                        {
+                          type:"expression",
+                          // minValue: 0,
+                          expression: '{panel.quantity}<={panel.products.maximum}',
+                          text: "Please correct the quantity. The value exceeds our stock."
+                        }
+                      ]
+                  }, {type: "expression",
+                      name: "cost",
+                      title: "Item Cost:",
+                      expression: '{panel.products.cost}',
+                      displayStyle: "currency",
+                      currency: "INR",
+                      startWithNewLine: false
+                  },{type: "expression",
+                      name: "total",
+                      title: "Total Item Cost:",
+                      expression: '{panel.quantity}*{panel.cost}',
+                      displayStyle: "currency",
+                      currency: "EUR",
+                      startWithNewLine: false
+                  }
+              ],
+              minPanelCount: 1,
+              panelAddText: "Add another  item",
+              panelRemoveText: "Remove item"
+          }, {
+              type: "panel",
+              title: "Totals",
+              elements: [
+                  {
+                      type: "expression",
+                      name: "totalQuantity",
+                      title: "Total  Quantity:",
+                      expression: "sumInArray({items}, 'quantity')"
+                  }, {
+                      type: "expression",
+                      name: "totalCost",
+                      title: "Total Cost:",
+                      expression: "sumInArray({items}, 'total')",
+                      displayStyle: "currency",
+                      currency: "INR",
+                      startWithNewLine: false
+                  }
+              ]
+            }
+          ]
+        };
 
-  methods: {
-    getCity (stateName) {
-      let stateAbbr = stateName
-      
-      let headers = new Headers();
-      headers.append("X-CSCAPI-KEY", "YXFqZEJsWWpEbFFzSFNBb0ZKNkhTRVZ2SEhlc1dxUTBsOEd3dlhERA==");
-      const  requestOptions = {
-        method: 'GET',
-        headers: headers,
-        redirect: 'follow'
-      };
+        SurveyVue.ChoicesRestfull.onBeforeSendRequest = function(sender, options) {
+          options.request.setRequestHeader("X-CSCAPI-KEY","YXFqZEJsWWpEbFFzSFNBb0ZKNkhTRVZ2SEhlc1dxUTBsOEd3dlhERA==");
+        };
 
-      fetch(`https://api.countrystatecity.in/v1/countries/IN/states/${stateAbbr}/cities`, requestOptions)
-        .then(response =>response.json())
-        .then(result=>this.cities=result)
-        .then(()=>{
-          let cityNames = []
-          this.cities.forEach(city=>{
-            cityNames.push(city.name)
-          })
-          // console.log(cityNames,'cityNames')
-          let q = this.survey.getQuestionByName('cities')
-          q.choices=cityNames
-          q
-            .choicesByUrl
-            .run()
-        })
-        .catch(error => console.log('error', error));
-    }
-  }
+        const model = new SurveyVue.Model(json)
+        this.survey = model
+      })
+      .catch(error => console.log('error', error));
+    },
 }
 </script>
-
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
